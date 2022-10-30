8d9dc21b26610208a5b2e11ce9234241a1a1814f
Jev99 <catforward.5711@gmail.com>
2022-10-30 15:33:45 +0300

    grisha

--- projects/Patrikeyev/test_lesson_1/Program.cs
+++ projects/Patrikeyev/test_lesson_1/Program.cs
@@ -1,29 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
 namespace ConsoleApplication1
 {
+    class Human
+    {
+        public string name;
+        public int age;
+        public int height;
+    }
+    class Child : Human
+    {
+        new public int age = 5;
+        new public int height = 100;
+
+        public void SetAge(int x)
+        {
+            if ((x > 0) & (x < 16))
+            {
+                age = x;
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Height: " + height);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            string poisk = "5";
-
-            Console.WriteLine("Enter a string");
-
-            string str = Console.ReadLine();
-            int dlina = str.Length;
-
-            for (int i = 0; i < dlina; i++)
-            {
-                if (str[i].ToString() == poisk)
-                {
-                    Console.WriteLine(poisk + " on = " + i + " position");
-                }
-            }
-            Console.ReadKey();
+            Child Ivan = new Child();
+            Ivan.Print();
+            Console.ReadLine();
         }
     }
 }
